whot/libwacom

commit b6b82e4afa6a2114afc819cddf6b3ee65dff2e15

Add support for relative dials (#665)
We already have a number of tablets with a relative dial that sends
mouse wheel events (UCLOGIC/Huion/XP Pen devices, primarily) so let's
add support for these in our description files.

This is copy/paste from the touch strip and ring support.

libwacom/libwacom-database.c
@@ -408,7 +408,9 @@ static const struct {
 	{ "Ring2", WACOM_BUTTON_RING2_MODESWITCH },
 	{ "Touchstrip", WACOM_BUTTON_TOUCHSTRIP_MODESWITCH },
 	{ "Touchstrip2", WACOM_BUTTON_TOUCHSTRIP2_MODESWITCH },
-	{ "OLEDs", WACOM_BUTTON_OLED }
+	{ "OLEDs", WACOM_BUTTON_OLED },
+	{ "Dial", WACOM_BUTTON_DIAL_MODESWITCH },
+	{ "Dial2", WACOM_BUTTON_DIAL2_MODESWITCH },
 };
 
 static const struct {
@@ -671,6 +673,8 @@ libwacom_parse_buttons(WacomDevice *device,
 	device->ring_num_modes = libwacom_parse_num_modes(device, keyfile, "RingNumModes", WACOM_BUTTON_RING_MODESWITCH);
 	device->ring2_num_modes = libwacom_parse_num_modes(device, keyfile, "Ring2NumModes", WACOM_BUTTON_RING2_MODESWITCH);
 	device->strips_num_modes = libwacom_parse_num_modes(device, keyfile, "StripsNumModes", WACOM_BUTTON_TOUCHSTRIP_MODESWITCH);
+	device->dial_num_modes = libwacom_parse_num_modes(device, keyfile, "DialNumModes", WACOM_BUTTON_DIAL_MODESWITCH);
+	device->dial2_num_modes = libwacom_parse_num_modes(device, keyfile, "Dial2NumModes", WACOM_BUTTON_DIAL2_MODESWITCH);
 }
 
 static void
@@ -771,6 +775,7 @@ libwacom_parse_features(WacomDevice *device, GKeyFile *keyfile)
 
 	device->num_rings = g_key_file_get_integer(keyfile, FEATURES_GROUP, "NumRings", NULL);
 	device->num_strips = g_key_file_get_integer(keyfile, FEATURES_GROUP, "NumStrips", NULL);
+	device->num_dials = g_key_file_get_integer(keyfile, FEATURES_GROUP, "NumDials", NULL);
 
 	string_list = g_key_file_get_string_list(keyfile, FEATURES_GROUP, "StatusLEDs", NULL, NULL);
 	if (string_list) {
@@ -907,6 +912,7 @@ libwacom_parse_tablet_keyfile(WacomDeviceDatabase *db,
 	}
 
 	device->num_strips = g_key_file_get_integer(keyfile, FEATURES_GROUP, "NumStrips", NULL);
+	device->num_dials = g_key_file_get_integer(keyfile, FEATURES_GROUP, "NumDials", NULL);
 	device->buttons = g_hash_table_new_full(g_direct_hash, g_direct_equal,
 						NULL, g_free);
 	device->status_leds = g_array_new (FALSE, FALSE, sizeof(WacomStatusLEDs));

libwacom/libwacom.c
@@ -404,8 +404,11 @@ libwacom_copy(const WacomDevice *device)
 	d->cls = device->cls;
 	d->num_strips = device->num_strips;
 	d->num_rings = device->num_rings;
+	d->num_dials = device->num_dials;
 	d->features = device->features;
 	d->strips_num_modes = device->strips_num_modes;
+	d->dial_num_modes = device->dial_num_modes;
+	d->dial2_num_modes = device->dial2_num_modes;
 	d->ring_num_modes = device->ring_num_modes;
 	d->ring2_num_modes = device->ring2_num_modes;
 	d->styli = g_array_sized_new(FALSE, FALSE, sizeof(int),
@@ -522,10 +525,19 @@ libwacom_compare(const WacomDevice *a, const WacomDevice *b, WacomCompareFlags f
 	if (a->num_strips != b->num_strips)
 		return 1;
 
+	if (a->num_dials != b->num_dials)
+		return 1;
+
 	if (a->features != b->features)
 		return 1;
 
 	if (a->strips_num_modes != b->strips_num_modes)
+		return 1;
+
+	if (a->dial_num_modes != b->dial_num_modes)
+		return 1;
+
+	if (a->dial2_num_modes != b->dial2_num_modes)
 		return 1;
 
 	if (a->ring_num_modes != b->ring_num_modes)
@@ -843,13 +855,18 @@ static void print_buttons_for_device (int fd, const WacomDevice *device)
 	print_button_flag_if(fd, device, "Bottom", WACOM_BUTTON_POSITION_BOTTOM);
 	print_button_flag_if(fd, device, "Touchstrip", WACOM_BUTTON_TOUCHSTRIP_MODESWITCH);
 	print_button_flag_if(fd, device, "Touchstrip2", WACOM_BUTTON_TOUCHSTRIP2_MODESWITCH);
+	print_button_flag_if(fd, device, "Dial", WACOM_BUTTON_DIAL_MODESWITCH);
 	print_button_flag_if(fd, device, "OLEDs", WACOM_BUTTON_OLED);
 	print_button_flag_if(fd, device, "Ring", WACOM_BUTTON_RING_MODESWITCH);
 	print_button_flag_if(fd, device, "Ring2", WACOM_BUTTON_RING2_MODESWITCH);
+	print_button_flag_if(fd, device, "Dial", WACOM_BUTTON_DIAL_MODESWITCH);
+	print_button_flag_if(fd, device, "Dial2", WACOM_BUTTON_DIAL2_MODESWITCH);
 	print_button_evdev_codes(fd, device);
 	dprintf(fd, "RingNumModes=%d\n", libwacom_get_ring_num_modes(device));
 	dprintf(fd, "Ring2NumModes=%d\n", libwacom_get_ring2_num_modes(device));
 	dprintf(fd, "StripsNumModes=%d\n", libwacom_get_strips_num_modes(device));
+	dprintf(fd, "DialNumModes=%d\n", libwacom_get_dial_num_modes(device));
+	dprintf(fd, "Dial2NumModes=%d\n", libwacom_get_dial2_num_modes(device));
 
 	dprintf(fd, "\n");
 }
@@ -951,6 +968,9 @@ libwacom_print_device_description(int fd, const WacomDevice *device)
 	print_supported_leds(fd, device);
 
 	dprintf(fd, "NumStrips=%d\n",	libwacom_get_num_strips(device));
+	dprintf(fd, "\n");
+
+	dprintf(fd, "NumDials=%d\n",	libwacom_get_num_dials(device));
 	dprintf(fd, "\n");
 
 	print_buttons_for_device(fd, device);
@@ -1242,6 +1262,24 @@ LIBWACOM_EXPORT int
 libwacom_get_strips_num_modes(const WacomDevice *device)
 {
 	return device->strips_num_modes;
+}
+
+LIBWACOM_EXPORT int
+libwacom_get_num_dials(const WacomDevice *device)
+{
+	return device->num_dials;
+}
+
+LIBWACOM_EXPORT int
+libwacom_get_dial_num_modes(const WacomDevice *device)
+{
+	return device->dial_num_modes;
+}
+
+LIBWACOM_EXPORT int
+libwacom_get_dial2_num_modes(const WacomDevice *device)
+{
+	return device->dial2_num_modes;
 }
 
 LIBWACOM_EXPORT const WacomStatusLEDs *

libwacom/libwacom.h
@@ -231,10 +231,13 @@ typedef enum {
 	WACOM_BUTTON_TOUCHSTRIP_MODESWITCH  = (1 << 7),
 	WACOM_BUTTON_TOUCHSTRIP2_MODESWITCH = (1 << 8),
 	WACOM_BUTTON_OLED                   = (1 << 9),
-	WACOM_BUTTON_MODESWITCH             = (WACOM_BUTTON_RING_MODESWITCH | WACOM_BUTTON_RING2_MODESWITCH | WACOM_BUTTON_TOUCHSTRIP_MODESWITCH | WACOM_BUTTON_TOUCHSTRIP2_MODESWITCH),
+	WACOM_BUTTON_DIAL_MODESWITCH        = (1 << 10),
+	WACOM_BUTTON_DIAL2_MODESWITCH       = (1 << 11),
+	WACOM_BUTTON_MODESWITCH             = (WACOM_BUTTON_RING_MODESWITCH | WACOM_BUTTON_RING2_MODESWITCH | WACOM_BUTTON_TOUCHSTRIP_MODESWITCH | WACOM_BUTTON_TOUCHSTRIP2_MODESWITCH | WACOM_BUTTON_DIAL_MODESWITCH | WACOM_BUTTON_DIAL2_MODESWITCH),
 	WACOM_BUTTON_DIRECTION              = (WACOM_BUTTON_POSITION_LEFT | WACOM_BUTTON_POSITION_RIGHT | WACOM_BUTTON_POSITION_TOP | WACOM_BUTTON_POSITION_BOTTOM),
 	WACOM_BUTTON_RINGS_MODESWITCH       = (WACOM_BUTTON_RING_MODESWITCH | WACOM_BUTTON_RING2_MODESWITCH),
 	WACOM_BUTTON_TOUCHSTRIPS_MODESWITCH = (WACOM_BUTTON_TOUCHSTRIP_MODESWITCH | WACOM_BUTTON_TOUCHSTRIP2_MODESWITCH),
+	WACOM_BUTTON_DIALS_MODESWITCH       = (WACOM_BUTTON_DIAL_MODESWITCH | WACOM_BUTTON_DIAL2_MODESWITCH),
 } WacomButtonFlags;
 
 /**
@@ -681,6 +684,31 @@ int libwacom_get_num_strips(const WacomDevice *device);
  * @ingroup devices
  */
 int libwacom_get_strips_num_modes(const WacomDevice *device);
+
+/**
+ * @param device The tablet to query
+ * @return the number of rotary dials on the tablet
+ * otherwise
+ *
+ * @ingroup devices
+ */
+int libwacom_get_num_dials(const WacomDevice *device);
+
+/**
+ * @param device The tablet to query
+ * @return the number of modes for the dial, if any
+ *
+ * @ingroup devices
+ */
+int libwacom_get_dial_num_modes(const WacomDevice *device);
+
+/**
+ * @param device The tablet to query
+ * @return the number of modes for the second dial, if any
+ *
+ * @ingroup devices
+ */
+int libwacom_get_dial2_num_modes(const WacomDevice *device);
 
 /**
  * @param device The tablet to query

tools/debug-device.c
@@ -162,6 +162,9 @@ handle_device(WacomDeviceDatabase *db, const char *path)
 	intfunc(libwacom_get_ring2_num_modes, device);
 	intfunc(libwacom_get_num_strips, device);
 	intfunc(libwacom_get_strips_num_modes, device);
+	intfunc(libwacom_get_num_dials, device);
+	intfunc(libwacom_get_dial_num_modes, device);
+	intfunc(libwacom_get_dial2_num_modes, device);
 
 	{
 		WacomIntegrationFlags flags = libwacom_get_integration_flags(device);
@@ -190,7 +193,7 @@ handle_device(WacomDeviceDatabase *db, const char *path)
 			WacomButtonFlags flags;
 
 			flags = libwacom_get_button_flag(device, b);
-			func_arg(libwacom_get_button_flag, "%c", b, "%s%s%s%s%s%s%s%s%s%s",
+			func_arg(libwacom_get_button_flag, "%c", b, "%s%s%s%s%s%s%s%s%s%s%s%s",
 				 flags == WACOM_BUTTON_NONE ? "NONE" : "",
 				 flags & WACOM_BUTTON_POSITION_LEFT ? "POSITION_LEFT|" : "",
 				 flags & WACOM_BUTTON_POSITION_RIGHT ? "POSITION_RIGHT|" : "",
@@ -200,8 +203,9 @@ handle_device(WacomDeviceDatabase *db, const char *path)
 				 flags & WACOM_BUTTON_RING2_MODESWITCH ? "RING2_MODESWITCH|" : "",
 				 flags & WACOM_BUTTON_TOUCHSTRIP_MODESWITCH ? "TOUCHSTRIP_MODESWITCH|" : "",
 				 flags & WACOM_BUTTON_TOUCHSTRIP2_MODESWITCH ? "TOUCHSTRIP2_MODESWITCH|" : "",
+				 flags & WACOM_BUTTON_DIAL_MODESWITCH ? "DIAL_MODESWITCH|" : "",
+				 flags & WACOM_BUTTON_DIAL2_MODESWITCH ? "DIAL2_MODESWITCH|" : "",
 				 flags & WACOM_BUTTON_OLED ? "OLED " : "");
-
 		}
 	}
 

data/wacom.example
@@ -167,9 +167,13 @@ TouchSwitch=false
 
 # This tablet has one or more touch rings (Intuos4 and Cintiq 24HD)
 # A touch ring is a circular button that responds to touch
-# (rather than clicks):
+# (rather than clicks) and sends absolute events:
 # http://intuos.wacom.com/americas/touch-ring.php
 NumRings=1
+
+# This tablet's number of dials/rotary toggles that send relative events
+# similar to a mouse wheel.
+NumDials=1
 
 # This tablet's number of touch strips (e.g. Cintiq 22HD), default is zero
 NumStrips=1
@@ -219,6 +223,15 @@ Touchstrip2=J
 # We assume the same number of modes for each of the touchstrips
 # if there is more than one
 StripsNumModes=4
+
+# If the dials have mode toggling through a button
+Dial=A
+# The number of modes we can toggle through.
+DialNumModes=4
+# Buttons associated with the second dial, if any.
+Dial2=B
+# Number of modes on the second dial, if any
+Dial2NumModes=4
 
 
 # Metadata about the keys on the tablet

libwacom/libwacom.sym
@@ -74,6 +74,9 @@ LIBWACOM_2.9 {
 } LIBWACOM_2.0;
 
 LIBWACOM_2.12 {
-    libwacom_match_get_uniq;
+    libwacom_get_dial_num_modes;
+    libwacom_get_dial2_num_modes;
+    libwacom_get_num_dials;
     libwacom_get_num_rings;
+    libwacom_match_get_uniq;
 } LIBWACOM_2.9;

libwacom/libwacomint.h
@@ -88,10 +88,13 @@ struct _WacomDevice {
 	WacomClass cls;
 	int num_strips;
 	int num_rings;
+	int num_dials;
 	uint32_t features;
 	uint32_t integration_flags;
 
 	int strips_num_modes;
+	int dial_num_modes;
+	int dial2_num_modes;
 	int ring_num_modes;
 	int ring2_num_modes;
 

test/test-load.c
@@ -126,6 +126,7 @@ test_intuos4(struct fixture *f, gconstpointer user_data)
 	g_assert_cmpint(libwacom_get_num_rings(device), ==, 1);
 	g_assert_false(libwacom_has_touchswitch(device));
 	g_assert_cmpint(libwacom_get_num_strips(device), ==, 0);
+	g_assert_cmpint(libwacom_get_num_dials(device), ==, 0);
 	g_assert_cmpint(libwacom_get_integration_flags (device), ==, WACOM_DEVICE_INTEGRATED_NONE);
 	g_assert_cmpint(libwacom_get_width(device), ==, 8);
 	g_assert_cmpint(libwacom_get_height(device), ==, 5);
@@ -177,6 +178,7 @@ test_cintiq21ux(struct fixture *f, gconstpointer user_data)
 	g_assert_nonnull(device);
 
 	g_assert_cmpint(libwacom_get_num_strips(device), ==, 2);
+	g_assert_cmpint(libwacom_get_num_dials(device), ==, 0);
 	libwacom_destroy(device);
 }
 

commit 77e3e60d16c3eeb1a22131638e20a8d433a7acf6

Add support for Dial status LEDs
Fixes: b6b82e4afa6a ("Add support for relative dials (#665)")

libwacom/libwacom-database.c
@@ -440,7 +440,9 @@ static const struct {
 	{ "Ring",		WACOM_STATUS_LED_RING },
 	{ "Ring2",		WACOM_STATUS_LED_RING2 },
 	{ "Touchstrip",		WACOM_STATUS_LED_TOUCHSTRIP },
-	{ "Touchstrip2",	WACOM_STATUS_LED_TOUCHSTRIP2 }
+	{ "Touchstrip2",	WACOM_STATUS_LED_TOUCHSTRIP2 },
+	{ "Dial",		WACOM_STATUS_LED_DIAL },
+	{ "Dial2",		WACOM_STATUS_LED_DIAL2 },
 };
 
 static const struct {

libwacom/libwacom.c
@@ -1487,7 +1487,9 @@ static const struct {
 	{ WACOM_BUTTON_RING_MODESWITCH,		WACOM_STATUS_LED_RING },
 	{ WACOM_BUTTON_RING2_MODESWITCH,	WACOM_STATUS_LED_RING2 },
 	{ WACOM_BUTTON_TOUCHSTRIP_MODESWITCH,	WACOM_STATUS_LED_TOUCHSTRIP },
-	{ WACOM_BUTTON_TOUCHSTRIP2_MODESWITCH,	WACOM_STATUS_LED_TOUCHSTRIP2 }
+	{ WACOM_BUTTON_TOUCHSTRIP2_MODESWITCH,	WACOM_STATUS_LED_TOUCHSTRIP2 },
+	{ WACOM_BUTTON_DIAL_MODESWITCH,		WACOM_STATUS_LED_DIAL },
+	{ WACOM_BUTTON_DIAL2_MODESWITCH,	WACOM_STATUS_LED_DIAL2 },
 };
 
 LIBWACOM_EXPORT int

libwacom/libwacom.h
@@ -304,7 +304,9 @@ typedef enum {
 	WACOM_STATUS_LED_RING		= 0,
 	WACOM_STATUS_LED_RING2		= 1,
 	WACOM_STATUS_LED_TOUCHSTRIP	= 2,
-	WACOM_STATUS_LED_TOUCHSTRIP2	= 3
+	WACOM_STATUS_LED_TOUCHSTRIP2	= 3,
+	WACOM_STATUS_LED_DIAL		= 4,
+	WACOM_STATUS_LED_DIAL2		= 5,
 } WacomStatusLEDs;
 
 /**

tools/debug-device.c
@@ -222,6 +222,8 @@ handle_device(WacomDeviceDatabase *db, const char *path)
 				case WACOM_STATUS_LED_RING2: ledstr = "RING2"; break;
 				case WACOM_STATUS_LED_TOUCHSTRIP: ledstr = "TOUCHSTRIP"; break;
 				case WACOM_STATUS_LED_TOUCHSTRIP2: ledstr = "TOUCHSTRIP2"; break;
+				case WACOM_STATUS_LED_DIAL: ledstr = "DIAL"; break;
+				case WACOM_STATUS_LED_DIAL2: ledstr = "DIAL2"; break;
 			}
 			snprintf(buf + strlen(buf), sizeof(buf) - strlen(buf), "%s%s", i > 0 ? ", " : "", ledstr);
 		}

test/__init__.py
@@ -218,7 +218,7 @@ class LibWacom:
         _Api(
             name="libwacom_get_status_leds",
             args=(c_void_p, c_void_p),
-            return_type=c_void_p,
+            return_type=ctypes.POINTER(ctypes.c_int),
         ),
         _Api(
             name="libwacom_get_button_led_group",
@@ -394,6 +394,8 @@ class LibWacom:
         _Enum(name="WACOM_STATUS_LED_RING2", value=2),
         _Enum(name="WACOM_STATUS_LED_TOUCHSTRIP", value=3),
         _Enum(name="WACOM_STATUS_LED_TOUCHSTRIP2", value=4),
+        _Enum(name="WACOM_STATUS_LED_DIAL", value=1),
+        _Enum(name="WACOM_STATUS_LED_DIAL2", value=2),
     ]
 
 
@@ -616,6 +618,16 @@ class WacomStylus:
         return styli
 
 
+class WacomStatusLed(enum.IntEnum):
+    UNAVAILABLE = -1
+    RING = 0
+    RING2 = 1
+    TOUCHSTRIP = 2
+    TOUCHSTRIP2 = 3
+    DIAL = 4
+    DIAL2 = 5
+
+
 class WacomDevice:
     """
     Convenience wrapper to make using libwacom a bit more pythonic.
@@ -794,6 +806,17 @@ class WacomDevice:
 
     def button_evdev_code(self, button: str) -> int:
         return self.get_button_evdev_code(button.encode("utf-8"))
+
+    def button_led_group(self, button: str) -> List[ButtonFlags]:
+        return self.get_button_led_group(button.encode("utf-8"))
+
+    @property
+    def status_leds(self) -> List["WacomStatusLed"]:
+        nleds = c_int()
+        leds = self.get_status_leds(ctypes.byref(nleds))
+        # leds = ctypes.cast(leds, ctypes.POINTER(ctypes.c_int))
+
+        return [WacomStatusLed(l) for l in leds[: nleds.value]]
 
 
 class WacomDatabase:

test/test_libwacom.py
@@ -8,7 +8,14 @@ from dataclasses import dataclass, field
 import logging
 import pytest
 
-from . import WacomBuilder, WacomBustype, WacomDatabase, WacomDevice, WacomEraserType
+from . import (
+    WacomBuilder,
+    WacomBustype,
+    WacomDatabase,
+    WacomDevice,
+    WacomEraserType,
+    WacomStatusLed,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -602,6 +609,76 @@ def test_button_modeswitch(custom_datadir, feature, count):
     for b in "ABCEF":
         flags = device.button_flags(b)
         assert expected_flag not in flags
+
+
+@pytest.mark.parametrize(
+    "feature",
+    ("Ring", "Touchstrip", "Dial"),
+)
+@pytest.mark.parametrize("count", (1, 2))
+def test_status_leds(custom_datadir, feature, count):
+    USBID = (0x1234, 0x5678)
+
+    # sigh, Touchstrip but StripsNumModes...
+    num_mode_key = f"{feature}s" if feature != "Touchstrip" else "Strips"
+
+    extra = {
+        "Buttons": {
+            "Left": "A;B;C;",
+            "Right": "D;E;F;",
+            feature: "A",
+        },
+        "Features": {
+            "StatusLEDs": f"{feature};{feature}2" if count > 1 else f"{feature}",
+            num_mode_key: 4,
+        },
+    }
+    if count > 1:
+        extra["Buttons"][f"{feature}2"] = "D"
+
+    TabletFile(
+        name="some tablet",
+        matches=[f"usb|{USBID[0]:04x}|{USBID[1]:04x}"],
+        extra=extra,
+    ).write_to(custom_datadir / "led.tablet")
+
+    db = WacomDatabase(path=custom_datadir)
+    builder = WacomBuilder.create(usbid=USBID)
+    device = db.new_from_builder(builder)
+    assert device is not None
+
+    expected = [
+        {
+            "Ring": WacomStatusLed.RING,
+            "Touchstrip": WacomStatusLed.TOUCHSTRIP,
+            "Dial": WacomStatusLed.DIAL,
+        }[feature]
+    ]
+
+    if count > 1:
+        expected.append(
+            {
+                "Ring": WacomStatusLed.RING2,
+                "Touchstrip": WacomStatusLed.TOUCHSTRIP2,
+                "Dial": WacomStatusLed.DIAL2,
+            }[feature]
+        )
+
+    leds = device.status_leds
+    assert sorted(leds) == sorted(expected)
+
+    led_group = device.button_led_group("A")
+    assert led_group == 0
+
+    led_group = device.button_led_group("D")
+    if count > 1:
+        assert led_group == 1
+    else:
+        assert led_group == -1
+
+    for b in "BCEF":
+        led_group = device.button_led_group(b)
+        assert led_group == -1
 
 
 def test_nonwacom_stylus_ids(tmp_path):